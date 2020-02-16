b4e3dff498845a9232481b2d85fd49dd767829c9
池田慎哉 <ikeshin0509@gmail.com>
2020-02-17 00:25:30 +0900

    7-10 課題提出　
    キューブが地面、並びにキューブ同士でぶつかった時に効果音を出すスクリプトの構築

---
+++ Assets/CubeController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour 
+{
+	// キューブの移動速度
+	private float speed = -12;
+
+	// 消滅位置
+	private float deadLine = -10;
+
+	//キューブが地面と接するときの音　
+	AudioSource myaudiosource;
+
+
+	// Use this for initialization
+	void Start()
+	{
+		//オーディオコンポーネントのの取得　
+		this.myaudiosource = GetComponent<AudioSource> (); 
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		// キューブを移動させる
+		transform.Translate (this.speed* Time.deltaTime, 0, 0);
+
+		// 画面外に出たら破棄する
+		if (transform.position.x < this.deadLine)
+		{
+			Destroy (gameObject);
+		}
+	
+	
+	}
+
+	//キューブが地面と接触しているとき
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.tag == "GrTag")
+		{
+			//音を鳴らす　
+			myaudiosource.Play ();
+
+		}
+
+		if (other.gameObject.tag == "CubeTag")
+		{
+			//音を鳴らす　
+			myaudiosource.Play ();
+
+		}
+	}
+
+}
